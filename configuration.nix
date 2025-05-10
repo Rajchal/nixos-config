1768541dc71fc38c6c62167b0d0a2a0f03bf87dd
Rajchal <rajchalanjal1@gmail.com>
2025-05-10 21:36:40 +0545

    config up

--- configuration.nix
+++ configuration.nix
@@ -120,5 +120,6 @@ networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
+  nixpkgs.config.allowUnfree = true;
 
 }
